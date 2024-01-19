925542f2b31f63821cba05ab4401116dc97af157
HBous <122835950+HOUBOUS@users.noreply.github.com>
2024-01-19 03:25:16 +0100

    Configuration contact page

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import './Presentation.css';
+import React from "react";
+import "./Presentation.css";
+import CvIcon from '../../assets/curriculum-vitae.png';
 
 function Presentation() {
   return (
-    <div id='presentation'> 
-  
-    <p className='presentation-text'> Hi, im !!</p>
-    <h1>Houda Bouslama!</h1>
-    <div id='my-presentation'>
-      <span id='my-presentation-one'></span>
-      
-      <span  id='my-presentation-two'></span>
-    </div>
-    
-    <h2>Intégrateur web   <br/> 
-    
-        Developpeuse Front-End
-    
-    </h2>
+    <div id="presentation">
+      Hi, I'm!
+        <p className="presentation-text">
+        <a href="https://www.canva.com/design/DAF6O30rwZs/view" target="_blank" rel="noopener noreferrer">
+        <img src={CvIcon} alt="CV Icon" className="cv-icon" />
+        </a>
+      </p>
+     
+      <h1>Houda Bouslama!</h1>
+      <div id="my-presentation">
+        <span id="my-presentation-one"></span>
 
-    
-    
-    
+        <span id="my-presentation-two"></span>
+      </div>
+
+      <h2>
+        Intégrateur web <br />
+        Developpeuse Front-End
+      </h2>
     </div>
-  )
+  );
 }
 
-export default Presentation
+export default Presentation;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Contact.css";
 import GitHubIcon from '../../assets/githubIcon.png';
+import MailIcon from '../../assets/emailIcon.png';
+import PhoneIcon from '../../assets/phone.png';
+import LinkedinIcon from '../../assets/Linkedin.png';
 
 
 function Contact() {
@@ -12,30 +15,38 @@ function Contact() {
     <div className="container mt-5">
       <h2 className="mb-4">Coordonnées</h2>
 
-      <div className="row">
+      <div className="contact-items">
         <div className="col-md-6">
+          <section className="row contact-item">
           <p>
-            <img src="MailIcon" alt="" className="icon" />
+            <img src={MailIcon} alt="icon of mailing" className="icon" />
             <strong>Email:</strong> houda.bouslama81@hotmail.com
           </p>
+          </section>
+          <section className="row contact-item">
           <p>
-          <img src="PhoneIcon" alt="icon of phone" />
+          <img src={PhoneIcon} alt="icon of phone" className="icon"/>
             <strong>Téléphone:</strong> +33 6 67 60 36 11
           </p>
+          </section>
+          <section className="row contact-item link">
           <p>
-          <img src="LinkedinIcon" alt="icon of linkedin link" />
-            <strong>LinkedIn:</strong>{' '}
+          <img src={LinkedinIcon} alt="icon of linkedin link"  className="icon" />
+            <strong>LinkedIn :</strong>{' '}
             <a href="https://www.linkedin.com/in/houda-bouslama-9b64373b/" target="_blank" rel="noopener noreferrer">
-              Votre Profil LinkedIn
+            Mon Profil LinkedIn
             </a>
           </p>
+          </section>
+          <section className="row contact-item link">
           <p>
           <img src={GitHubIcon} alt="icon of gitHub link"  className="icon"/>
             <strong>GitHub:</strong>{' '}
             <a href="https://github.com/HOUBOUS" target="_blank" rel="noopener noreferrer">
-              Votre Profil GitHub
+              Mon Profil GitHub
             </a>
           </p>
+          </section>
         </div>
       </div>
     </div>
